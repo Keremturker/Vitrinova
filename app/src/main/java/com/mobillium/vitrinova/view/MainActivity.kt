1d02267eec8e324bd5b21407af280687c23bface
Kerem TÜRKER <iletisim@keremturker.com>
2021-02-22 17:56:14 +0300

    Kütüphane eklemeleri ve güncellemeleri yapıldı.

--- app/src/main/java/com/mobillium/vitrinova/view/MainActivity.kt
+++ app/src/main/java/com/mobillium/vitrinova/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mobillium.vitrinova
+package com.mobillium.vitrinova.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobillium.vitrinova.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
